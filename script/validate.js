a5911325938953e0e12dd8a201281d1be5cfbea6
Konstantin <altwebga@gmail.com>
2022-04-13 23:52:36 +0700

    v.1.4.4

--- script/validate.js
+++ script/validate.js
@@ -1,12 +1,12 @@
 const setInputValid = (config, errorMessage, input) => {
-  errorMessage.textContent = '';
+  errorMessage.textContent = "";
   input.classList.remove(config.inputErrorClass);
-}
+};
 
 const setInputInvalid = (config, errorMessage, input) => {
   errorMessage.textContent = input.validationMessage;
   input.classList.add(config.inputErrorClass);
-}
+};
 
 const checkInputValidity = (config, form, input) => {
   const errorMessage = form.querySelector(`#error-${input.id}`);
@@ -15,20 +15,20 @@ const checkInputValidity = (config, form, input) => {
   } else {
     setInputInvalid(config, errorMessage, input);
   }
-}
+};
 
 const hasInvalidInput = (inputList) => {
   return inputList.some((input) => {
     return !input.validity.valid;
-  })
+  });
 };
 
 const toggleButtonState = (config, inputList, button) => {
   if (hasInvalidInput(inputList)) {
-    button.setAttribute('disabled', '');
+    button.setAttribute("disabled", true);
     button.classList.add(config.inactiveButtonClass);
   } else {
-    button.removeAttribute('disabled');
+    button.removeAttribute("disabled");
     button.classList.remove(config.inactiveButtonClass);
   }
 };
@@ -37,31 +37,30 @@ const setEventListeners = (config, form) => {
   const inputList = Array.from(form.querySelectorAll(config.inputSelector));
   const button = form.querySelector(config.submitButtonSelector);
   inputList.forEach((input) => {
-    input.addEventListener('input', () => {
+    input.addEventListener("input", () => {
       checkInputValidity(config, form, input);
       toggleButtonState(config, inputList, button);
     });
   });
 };
 
-const enableValidation =(config) => {
+const enableValidation = (config) => {
   const formList = Array.from(document.querySelectorAll(config.formSelector));
 
-  formList.forEach(form => {
-    form.addEventListener('submit', (evt) => {
+  formList.forEach((form) => {
+    form.addEventListener("submit", (evt) => {
       evt.preventDefault();
-    })
+    });
 
     setEventListeners(config, form);
-  })
-}
+  });
+};
 
 enableValidation({
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__button-save',
-    inactiveButtonClass: 'popup__button-save_disabled',
-    inputErrorClass: 'popup__error',
-    errorClass: 'popup__error_visible',
-  });
-  
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button-save",
+  inactiveButtonClass: "popup__button-save_disabled",
+  inputErrorClass: "popup__error",
+  errorClass: "popup__error_visible",
+});
