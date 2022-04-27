8df6a420ad3a702141be4b32025917b8837d311f
Konstantin Kusnecov <altwebga@yandex.ru>
2022-04-27 17:24:32 +0700

    v.7.0

---
+++ script/FormValidator.js
@@ -0,0 +1,8 @@
+class FormValidator {
+    constructor(title, image, buttonTrash, buttonLike) {
+        this._title = title;
+        this._image = image;
+        this._buttonTrash = buttonTrash;
+        this._buttonLike = buttonLike;
+    }
+}
