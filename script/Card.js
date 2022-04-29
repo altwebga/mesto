c2461459eefa0f8adfaa8d7378683827a68941a9
Konstantin Kusnecov <altwebga@yandex.ru>
2022-04-29 15:50:19 +0700

    class card start

--- script/Card.js
+++ script/Card.js
@@ -1,8 +1,38 @@
 class Card {
-    constructor(title, image, buttonTrash, buttonLike) {
+    constructor(title, link) {
         this._title = title;
-        this._image = image;
-        this._buttonTrash = buttonTrash;
-        this._buttonLike = buttonLike;
+        this._link = link;
+
+    }
+
+    _getTemplate() {
+        const newCard = cardTemplate.querySelector(".card").cloneNode(true);
+        return newCard;
+    }
+    generateCard() {
+        const newCard = cardTemplate.querySelector(".card").cloneNode(true);
+
+        const image = newCard.querySelector(".card__image");
+        image.alt = title;
+        image.src = link;
+        image.addEventListener("click", openShowPhotoPopup);
+      
+        const title = newCard.querySelector(".card__title");
+        title.textContent = title;
+      
+        const like = newCard.querySelector(".card__button-like");
+        like.addEventListener("click", toggleLike);
+      
+        const trash = newCard.querySelector(".card__button-trash");
+        trash.addEventListener("click", deleteCard);
+      
+        return newCard;
     }
 }
+items.forEach((item) => {
+    const card = new Card(item.title, item.link,);
+    const cardElement = card.generateCard();
+  
+    // Добавляем в DOM
+    document.querySelector('.card').append(cardElement);
+  });
